d9c2482a881faa22b51ee5b5b6952bc4a4d9a7b9
Paul MARCOMBES <paul.marcombes@nickel.eu>
2023-08-31 17:59:34 +0200

    first build

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='simple_airbyte',
-    version='1.0',
-    py_modules=['simple_airbyte'],
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='simple_airbyte',                  # This is the name of the package
+    packages=['simple_airbyte'],
+    version='0.0.1',                        # The initial release version
+    author='Unytics',                       # Full name of the author
+    description='Airbyte made easy',
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.6',                # Minimum version requirement of the package
+    install_requires=[]                     # Install other dependencies if any
 )
