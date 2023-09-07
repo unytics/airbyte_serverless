8c6b13503892d609480353e1e5a514c1e61e5cfa
Paul MARCOMBES <paul.marcombes@nickel.eu>
2023-09-08 00:04:39 +0200

    cli initialized!

--- airbyte_serverless/cli.py
+++ airbyte_serverless/cli.py
@@ -1,3 +1,6 @@
+import sys
+import shutil
+
 import click
 from click_help_colors import HelpColorsGroup
 
@@ -17,6 +20,26 @@ def cli():
     pass
 
 
+def print_color(msg):
+    click.echo(click.style(msg, fg='cyan'))
+
+def print_success(msg):
+    click.echo(click.style(f'SUCCESS: {msg}', fg='green'))
+
+def print_info(msg):
+    click.echo(click.style(f'INFO: {msg}', fg='yellow'))
+
+def print_command(msg):
+    click.echo(click.style(f'INFO: `{msg}`', fg='magenta'))
+
+def print_warning(msg):
+    click.echo(click.style(f'WARNING: {msg}', fg='cyan'))
+
+def handle_error(msg):
+    click.echo(click.style(f'ERROR: {msg}', fg='red'))
+    sys.exit()
+
+
 @cli.command()
 @click.argument('connection')
 @click.option('--source', default='airbyte/source-faker:0.1.4', help='Any Public Docker Airbyte Source. Example: `airbyte/source-faker:0.1.4`. (connectors list: https://hub.docker.com/search?q=airbyte%2Fsource-')
@@ -25,4 +48,7 @@ def create(connection, source, destination):
     '''
     Create CONNECTION
     '''
-    print('hello')
+    if shutil.which('docker') is None:
+        handle_error('`docker` is not installed')
+    source = sources.AirbyteSource(source)
+    print(source.config)
