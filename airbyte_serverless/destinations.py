9a82397f77e481857e7d549cddda1f3a7b280be2
Paul MARCOMBES <paul.marcombes@nickel.eu>
2023-09-12 15:23:34 +0200

    destination: base destination does more stuff

--- airbyte_serverless/destinations.py
+++ airbyte_serverless/destinations.py
@@ -5,8 +5,17 @@ import uuid
 
 class BaseDestination:
 
-    def __init__(self, **kwargs):
-        raise NotImplementedError()
+    destination_columns = [
+        ('_airbyte_raw_id',           'string',    'Record uuid generated at ingestion'),
+        ('_airbyte_job_started_at',   'timestamp', 'Extract-load job start timestamp'),
+        ('_airbyte_slice_started_at', 'timestamp', 'When incremental mode is used, data records are emitted by chunks a.k.a. slices. At the end of each slice, a state record is emitted to store a checkpoint. This column stores the timestamp when the slice started'),
+        ('_airbyte_extracted_at',     'timestamp', 'Record extract timestamp from source'),
+        ('_airbyte_loaded_at',        'timestamp', 'Record ingestion timestamp'),
+        ('_airbyte_data',             'json',      'Record data as json'),
+    ]
+
+    def __init__(self, buffer_size_max=10000, **kwargs):
+        self.buffer_size_max = buffer_size_max
 
     def get_state(self):
         raise NotImplementedError()
@@ -15,18 +24,71 @@ class BaseDestination:
         raise NotImplementedError()
 
     def load(self, messages):
+        self.job_started_at = datetime.datetime.utcnow().isoformat()
+        self.slice_started_at = self.job_started_at
+        buffer = []
+        stream = None
+        for message in messages:
+            if message['type'] == 'RECORD':
+                new_stream = message['record']['stream']
+                if new_stream != stream and stream is not None:
+                    self._format_and_write(f'_airbyte_raw_{stream}', buffer)
+                    buffer = []
+                    self.slice_started_at = datetime.datetime.utcnow().isoformat()
+                stream = new_stream
+                buffer.append(message['record'])
+                if len(buffer) > self.buffer_size_max:
+                    self._format_and_write(f'_airbyte_raw_{stream}', buffer)
+                    buffer = []
+            elif message['type'] == 'STATE':
+                self._format_and_write(f'_airbyte_raw_{stream}', buffer)
+                buffer = []
+                self._format_and_write('_airbyte_states', [message['state']])
+                self.slice_started_at = datetime.datetime.utcnow().isoformat()
+            elif message['type'] == 'LOG':
+                self._format_and_write('_airbyte_logs', [message['log']])
+            elif message['type'] == 'TRACE':
+                self._format_and_write('_airbyte_logs', [message['trace']])
+            else:
+                raise NotImplementedError(f'message type {message["type"]} is not managed yet')
+        self._format_and_write(f'_airbyte_raw_{stream}', buffer)
+
+    def _format_and_write(self, record_type, records):
+        if not records:
+            return
+        records = self._format(record_type, records)
+        self._write(record_type, records)
+
+    def _format(self, record_type, records):
+        now  = datetime.datetime.utcnow().isoformat()
+        return [
+            {
+                '_airbyte_raw_id': str(uuid.uuid4()),
+                '_airbyte_job_started_at': self.job_started_at,
+                '_airbyte_slice_started_at': self.slice_started_at,
+                '_airbyte_extracted_at': record.get('emitted_at'),
+                '_airbyte_loaded_at': now,
+                '_airbyte_data': json.dumps(
+                    record['data'] if record_type.startswith('_airbyte_raw') else record,
+                    ensure_ascii=False
+                ),
+            }
+            for record in records
+        ]
+
+    def _write(self, record_type, records):
         raise NotImplementedError()
 
 
 class BigQueryDestination(BaseDestination):
 
-    def __init__(self, dataset='', buffer_size_max=10000):
+    def __init__(self, dataset='', **kwargs):
+        super().__init__(**kwargs)
         import google.cloud.bigquery
         assert dataset, 'dataset argument must be defined'
         assert len(dataset.split('.')) == 2, '`BigQueryDestination.dataset` must be like `project.dataset`'
         self.dataset = dataset
         self.project, _ = self.dataset.split('.')
-        self.buffer_size_max = buffer_size_max
         self.bigquery = google.cloud.bigquery.Client(project=self.project)
         self.created_tables = []
 
@@ -44,56 +106,9 @@ class BigQueryDestination(BaseDestination):
         rows = list(rows)
         return json.loads(rows[0].state) if rows else {}
 
-    def load(self, messages):
-        self.job_started_at = datetime.datetime.utcnow().isoformat()
-        self.slice_started_at = self.job_started_at
-        buffer = []
-        stream = None
-        for message in messages:
-            if message['type'] == 'RECORD':
-                new_stream = message['record']['stream']
-                if new_stream != stream and stream is not None:
-                    self._insert_rows(f'_airbyte_raw_{stream}', buffer)
-                    buffer = []
-                    self.slice_started_at = datetime.datetime.utcnow().isoformat()
-                stream = new_stream
-                buffer.append(message['record'])
-                if len(buffer) > self.buffer_size_max:
-                    self._insert_rows(f'_airbyte_raw_{stream}', buffer)
-                    buffer = []
-            elif message['type'] == 'STATE':
-                self._insert_rows(f'_airbyte_raw_{stream}', buffer)
-                buffer = []
-                self._insert_rows('_airbyte_states', [message['state']])
-                self.slice_started_at = datetime.datetime.utcnow().isoformat()
-            elif message['type'] == 'LOG':
-                self._insert_rows('_airbyte_logs', [message['log']])
-            elif message['type'] == 'TRACE':
-                self._insert_rows('_airbyte_logs', [message['trace']])
-            else:
-                raise NotImplementedError(f'message type {message["type"]} is not managed yet')
-        self._insert_rows(f'_airbyte_raw_{stream}', buffer)
-
-    def _insert_rows(self, record_type, records):
-        if not records:
-            return
+    def _write(self, record_type, records):
         table = record_type
         self._create_table_if_needed(table)
-        now  = datetime.datetime.utcnow().isoformat()
-        records = [
-            {
-                '_airbyte_raw_id': str(uuid.uuid4()),
-                '_airbyte_job_started_at': self.job_started_at,
-                '_airbyte_slice_started_at': self.slice_started_at,
-                '_airbyte_extracted_at': record.get('emitted_at'),
-                '_airbyte_loaded_at': now,
-                '_airbyte_data': json.dumps(
-                    record['data'] if record_type.startswith('_airbyte_raw') else record,
-                    ensure_ascii=False
-                ),
-            }
-            for record in records
-        ]
         errors = self.bigquery.insert_rows_json(f'{self.dataset}.{table}', records)
         if errors:
             raise ValueError(f'Could not insert rows to BigQuery table {table}. Errors: {errors}')
@@ -101,16 +116,15 @@ class BigQueryDestination(BaseDestination):
     def _create_table_if_needed(self, table):
         if table in self.created_tables:
             return
+        columns_definitions = ', '.join([
+            f'{column} {type} options(description="{description}")'
+            for column, type, description in self.destination_columns
+        ])
         self.bigquery.query(f'''
             create table if not exists {self.dataset}.{table} (
-                _airbyte_raw_id string options(description="Record uuid generated at insertion into BigQuery"),
-                _airbyte_job_started_at timestamp options(description="Extract-load job start timestamp"),
-                _airbyte_slice_started_at timestamp options(description="When incremental mode is used, data records are emitted by chunks a.k.a. slices. At the end of each slice, a state record is emitted to store a checkpoint. This column stores the timestamp when the slice started"),
-                _airbyte_extracted_at timestamp options(description="Record extract time from source"),
-                _airbyte_loaded_at timestamp options(description="Record ingestion time into BigQuery"),
-                _airbyte_data json options(description="Record data as json")
+                {columns_definitions}
             )
-            partition by date(_airbyte_emitted_at)
+            partition by date(_airbyte_loaded_at)
             options(
                 description="{table} records ingested by airbyte_serverless"
             )
