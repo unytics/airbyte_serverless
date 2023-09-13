cd9224cce6373798c9f42ea3be8eacd6541c3823
unytics <paul.marcombes@unytics.io>
2023-09-13 20:59:10 +0000

    v0.14 deployer minor

--- airbyte_serverless/deployers.py
+++ airbyte_serverless/deployers.py
@@ -6,7 +6,7 @@ import yaml
 DOCKERFILE = '''
 FROM {docker_image}
 
-RUN pip install airbyte-serverless
+RUN pip install airbyte-serverless==0.14
 
 ENV PYTHONUNBUFFERED True
 
@@ -18,6 +18,14 @@ ENTRYPOINT echo "salut" && abs run connection --from-deployed-docker-image
 
 '''
 
+def exec(command):
+    try:
+        print('Executing command >', command)
+        subprocess.run(command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        raise Exception('Error. See Above. ' + e.output.decode(errors='ignore').strip())
+
+
 class BaseDeployer:
     def __init__(self, connection):
         self.connection = connection
@@ -25,34 +33,28 @@ class BaseDeployer:
         self.folder = self.folder_obj.name        
 
     def deploy(self):
-        self._create_dockerfile()
-        self._deploy_dockerfile()
+        self.create_dockerfile()
+        self.deploy_dockerfile()
 
     def deploy_and_run(self):
         self.deploy()
         self.run()
 
-    def _create_dockerfile(self):
+    def create_dockerfile(self):
         dockerfile = DOCKERFILE.format(
             docker_image=self.connection.source.docker_image,
             connection_config=self.connection.yaml_config,
         )
         open(f'{self.folder}/Dockerfile', 'w', encoding='utf-8').write(dockerfile)
 
-    def _deploy_dockerfile(self):
-        try:
-            return subprocess.check_output(self.deploy_command, shell=True).decode().strip()
-        except subprocess.CalledProcessError as e:
-            raise Exception('Deploy Error. See Above. ' + e.output.decode(errors='ignore').strip())
+    def deploy_dockerfile(self):
+        exec(self.deploy_dockerfile_command)
 
     def run(self):
-        try:
-            return subprocess.check_output(self.run_command, shell=True).decode().strip()
-        except subprocess.CalledProcessError as e:
-            raise Exception('Run Error. See Above. ' + e.output.decode(errors='ignore').strip())
-                
+        exec(self.run_command)
+
     @property
-    def deploy_command(self):
+    def deploy_dockerfile_command(self):
         raise NotImplementedError()
 
     @property
@@ -63,7 +65,7 @@ class BaseDeployer:
 class LocalDockerDeployer(BaseDeployer):
    
     @property
-    def deploy_command(self):
+    def deploy_dockerfile_command(self):
         return f'docker build {self.folder} -t {self.connection.name}'        
         
     @property
@@ -74,6 +76,6 @@ class LocalDockerDeployer(BaseDeployer):
 class CloudRunJobDeployer(BaseDeployer):
 
     @property
-    def deploy_command(self):
+    def deploy_dockerfile_command(self):
         return f'gcloud run jobs deploy {self.connection_name} --source={self.folder} --execute-now --wait --project=bigfunctions --region=europe-west1 --max-retries=0'
 
